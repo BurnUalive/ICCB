da4c328c18f827c11277e24d731118ee0a3c9470
Ayush Agarwal <agarwalayush161@gmail.com>
2016-03-30 13:39:54 +0530

    Sign Up in Progress.

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
+var routes = require(path.join(__dirname, 'routes', 'index'));
+var users = require(path.join(__dirname, 'routes', 'users'));
+var mongo = require('express-mongo-db');
+var MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost/ICCB';
 var app = express();
 
 // view engine setup
@@ -21,6 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(mongo(MONGO_URL));
 
 app.use('/', routes);
 app.use('/users', users);
--- routes/index.js
+++ routes/index.js
@@ -32,4 +32,27 @@ router.get('/paperFull',function(req,res){
 router.get('/committees',function(req,res){
   res.render('committees');
 });
+router.post('/register', function (req, res) {
+  let data = {
+    name: req.body.name,
+    salutation: req.body.salutation,
+    designation: req.body.designation,
+    work_place: req.body.work_place,
+    email: req.body.email,
+    mobile: req.body.mobile
+  };
+  let db = req.db.collection('users');
+  const onInsert = function (err) {
+    if (!err) {
+      res.cookie('name', data.id, {maxAge: 86400000, signed: true});
+      res.redirect('/users');
+    }
+  };
+  db.count(function (err, count) {
+    count = count.toString();
+    for (i = 1; i < count.length; i++) {
+
+    }
+  });
+});
 module.exports = router;
