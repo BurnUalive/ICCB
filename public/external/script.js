c1480edb48da94b56ebf083d862d61397363522c
BurnUalive <burnualive@hotmail.com>
2016-04-16 18:21:34 +0530

    External control for vit.ac.in

---
+++ public/external/script.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Shivam Mathur on 16-04-2016.
+ */
+window.onload = function(){
+    var a = document.getElementsByTagName('iframe');
+    a.heigth = window.innerHeight+'px';
+};
+window.resize = function(){
+    var a = document.getElementsByTagName('iframe');
+    a.heigth = window.innerHeight+'px';
+};
