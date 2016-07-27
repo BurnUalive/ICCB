3757c9e711e4fd0ed9e6080b05d19c29a0192055
BurnUalive <burnualive@hotmail.com>
2016-07-27 23:05:09 +0530

    transactions

---
+++ db/mongo-transactions.js
@@ -0,0 +1,71 @@
+/**
+ * Created by Shivam Mathur on 27-07-2016.
+ */
+
+var i;
+var log;
+var flag;
+var collection;
+var path = require('path');
+
+var collection_name = 'transactions';
+
+exports.getCount = function (query,db, callback)
+{
+    collection = db.collection(collection_name);
+    var onFetch = function (err, count)
+    {
+        //db.close();
+        if (err)
+        {
+            callback(err);
+        }
+        else
+        {
+            callback(null, count);
+        }
+    };
+    collection.count(query, onFetch);
+
+};
+
+exports.insert = function (doc, db,callback)
+{
+    collection = db.collection(collection_name);
+    var onInsert = function (err, docs)
+    {
+        // db.close();
+        if (err)
+        {
+            callback(err, null);
+        }
+        else
+        {
+            callback(null, docs);
+        }
+    };
+    collection.insertOne(doc, {w: 1}, onInsert);
+};
+
+exports.fetch = function (doc,db, callback)
+{
+    collection = db.collection(collection_name);
+    var onFetch = function (err, document)
+    {
+        // db.close();
+        if (err)
+        {
+            callback(err, null);
+        }
+        else if (document && (doc['_id'] === document['_id']))
+        {
+            callback(null, document);
+        }
+        else
+        {
+            callback(false, null);
+        }
+    };
+    collection.findOne(doc, onFetch);
+
+};
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var http = require('http');
+var mongoTransaction = require(path.join(__dirname, '..', 'db', 'mongo-transactions'));
 
+var saleJSON ={
+    id_trans:'ICNAN',
+    id_event:process.env.id_event,
+    id_Merchant:process.env.id_Merchant,
+    id_Password:process.env.id_Password,
+    id_name:''
+};
+var zero = 00000000;
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     if (req.signedCookies.name)
@@ -13,6 +23,63 @@ router.get('/', function(req, res, next) {
         res.send({message:false});
     }
 
+});
+router.post('/sale',function(req,res){
+    if (req.signedCookies.name) {
+        var db = req.db;
+        var name = req.body.fullname;
+        var onGetCount = function (err, count) {
+            var num = zero + count;
+            var tempJson = saleJSON;
+            tempJson.id_trans = 'ICNAN' + num;
+            tempJson.id_name = name;
+            var data = querystring.stringify(tempJson);
+
+            var options = {
+                host: 'iccb.herokuapp.com',
+                port: 80,
+                path: process.env.path,
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Content-Length': Buffer.byteLength(data)
+                }
+            };
+
+            var httpreq = http.request(options, function (response) {
+                response.setEncoding('utf8');
+                response.on('data', function (chunk) {
+                    console.log("body: " + chunk);
+                });
+                response.on('end', function () {
+                    res.send('ok');
+                })
+            });
+            httpreq.write(data);
+            var onEnd = function (err, data) {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    console.log(data);
+                    res.render('confirm', {data: data});
+                }
+            };
+            httpreq.end(onEnd);
+        };
+        mongoTransaction.getCount({}, db, onGetCount);
+    }
+    else{
+        res.redirect('/userLogin');
+    }
+});
+router.get('/sell',function(req,res){
+    if(req.signedCookies.name) {
+        res.render('sell');
+    }
+    else{
+        res.redirect('/userLogin');
+    }
 });
 router.get('/logout',function(req,res){
     if (req.signedCookies.name)
