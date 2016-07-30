a46b8ea76278be58bae95cbdc0055e61d3b03190
Ayush Agarwal <agarwalayush161@gmail.com>
2016-07-30 16:52:12 +0530

    Added Init Payment Gateway Config.
    * added unirest module
    * configured the return URL and parameters
    * changed config vars on heroku

--- routes/users.js
+++ routes/users.js
@@ -2,16 +2,18 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 var http = require('http');
+var unirest = require('unirest');
+
 var querystring = require('querystring');
 var mongoTransaction = require(path.join(__dirname, '..', 'db', 'mongo-transactions'));
 
 var saleJSON ={
-    id_trans: 'ICNAN',
+    id_trans: 'ICCB',
     id_event: process.env.id_event,
     id_Merchant: process.env.id_Merchant,
-    id_Password: process.env.id_Password,
-    id_name: ''
+    id_Password: process.env.id_Password
 };
+
 var zero = 000000;
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -26,48 +28,31 @@ router.get('/', function(req, res, next) {
     }
 
 });
-router.get('/sale',function(req,res){
+router.post('/sale',function(req,res){
     if (req.signedCookies.name) {
         var db = req.db;
         var name = req.body.fullname;
         var onGetCount = function (err, count) {
             var num = zero + count;
-            var tempJson = saleJSON;
-            tempJson.id_trans = 'ICNAN' + num;
-            tempJson.id_name = name;
-            var data = querystring.stringify(tempJson);
-
-            var options = {
-                host: 'iccb.herokuapp.com',
-                port: 80,
-                path: process.env.path,
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                    'Content-Length': Buffer.byteLength(data)
-                }
+            var tId = 'ICCB' + num.toString;
+            var onEnd = function (response) {
+              if (response.error) {
+                console.log(response.error);
+                console.log('error occurred');
+              }
+              else {
+                res.render('payment', {html: response});
+              }
             };
-
-            var httpreq = http.request(options, function (response) {
-                response.setEncoding('utf8');
-                response.on('data', function (chunk) {
-                    console.log("body: " + chunk);
-                });
-                response.on('end', function () {
-                    res.send('ok');
-                })
-            });
-            httpreq.write(data);
-            var onEnd = function (err, data) {
-                if (err) {
-                    console.log(err);
-                }
-                else {
-                    console.log(data);
-                    res.render('confirm', {data: data});
-                }
-            };
-            httpreq.end(onEnd);
+            //TODO-calulate amount, currently hard coded as 1.00
+            unirest.post(process.env.payURL)
+              .send('id_trans=' + tId)
+              .send('id_event=' + process.env.id_event)
+              .send('id_merchant=' + process.env.id_merchant)
+              .send('id_password=' + process.env.id_password)
+              .send('id_name=' + name)
+              .send('amt_event=1.00')
+              .end(onEnd);
         };
         mongoTransaction.getCount({}, db, onGetCount);
     }
@@ -94,9 +79,17 @@ router.get('/logout',function(req,res){
 });
 
 router.post('/paymentComplete', function (req, res) {
-  var status = req.body.transactionID;
-  console.log(status);
-  res.redirect('/');
+  var refNo = req.body.Refno;
+  var tId = req.body.Tpsltranid;
+  var bankRefNo = req.body.bankrefno;
+  var txnDate = req.body.txndate;
+  var status = req.body.status;
+  if (tId && bankRefNo) {
+    console.log('success');
+  }
+  else {
+    console.log('fail');
+  }
 });
 
 module.exports = router;
