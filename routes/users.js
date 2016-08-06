15be88e80eaa448fe8612b8ca0b69e71b7e47d2f
Ayush Agarwal <agarwalayush161@gmail.com>
2016-08-06 18:17:11 +0530

    Error Fix

--- routes/users.js
+++ routes/users.js
@@ -76,8 +76,8 @@ router.get('/cred',function(req,res){
         }
         else{
             var tid = "ICCB"+count;
-            var name = req.body.name;
-            var amount = req.body.amt_event;
+            var name = req.params('name');
+            var amount = req.params('amt_event');
             var data = saleJSON;
             data.id_name = name;
             data.id_trans = tid;
