9fc892eed4b35e9521b2f607345503d44009891e
BurnUalive <burnualive@hotmail.com>
2016-09-03 11:59:42 +0530

    admin view shifts

--- public/javascripts/angular_input/controllers.js
+++ public/javascripts/angular_input/controllers.js
@@ -13,16 +13,18 @@ controllers.input = function ($scope, $http, FileUploader, $location) {
         $scope.state = user_login($http, checkState);
         $scope.admin = document.getElementById('test').value;
         console.log( document.getElementById('test').value);
-        $http({
-            method: 'GET',
-            url: '/input/getAllUsers'
-        }).success(function (data) {
-            $scope.users= data;
-            checkUserAbs();
 
-        });
         if($scope.admin==true)
-            {}else{
+            {
+                $http({
+                    method: 'GET',
+                    url: '/input/getAllUsers'
+                }).success(function (data) {
+                    $scope.users= data;
+                    checkUserAbs();
+
+                });
+            }else{
          //   checkSingle();
         }
     };
